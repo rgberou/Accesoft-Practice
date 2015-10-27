5a33adc95bbd2f61d3ca57e75489740b3893bb92
RgBerou <brotark@gmail.com>
2015-10-27 13:39:25 -0700

    Added new Files

---
+++ JS/connect.js
@@ -0,0 +1,22 @@
+(function(){
+	var app= angular.module('myapp',[]);
+	app.controller('FormCtrl',function($scope){
+	
+	
+	if(lname!=null){
+		var lname=$scope.lastname+",";
+	}
+	
+
+
+
+
+	
+	
+	
+	
+});
+
+})();
+
+
---
+++ connect.js
@@ -0,0 +1,10 @@
+
+
+
+var app= angular.module('FormCtrl',[]);
+
+app.controller(function($scope){
+	var lname=document.getElementById('lastname').value;
+
+	$scope.user="Argie";
+});
